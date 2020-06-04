3a75f50fbe6292b6a7054de980c157ece8866c7c
Vineet Madan <2017csb1119@iitrpr.ac.in>
2020-06-04 17:10:00 +0530

    added cdcrc logo

--- src/cdcrc_website/settings.py
+++ src/cdcrc_website/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'team',
     'recruiter',
     'profiles',
+    'info',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
--- src/cdcrc_website/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def homepage(request):
-    return render(request, 'home/home.html')
-
