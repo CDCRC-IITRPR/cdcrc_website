e5b37b76a240ea97c0332a67db30a5fced189eba
DhamiSamreet <56794225+DhamiSamreet@users.noreply.github.com>
2020-06-06 02:04:26 +0530

    models of news and events

--- src/info/views.py
+++ src/info/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import News,Events
 # Create your views here.
 def home(request):
-    return render(request, 'info/home.html')
+    news = News.objects.all()
+    events = Events.objects.all()
+    return render(request, 'info/home.html',{'news':news,'events':events})
+
 
 def contacts(request):
     return render(request, 'info/contacts.html')
