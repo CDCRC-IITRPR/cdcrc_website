aa4a4ef1b7eed5f9dd89713c248c748ef50c9eec
Ritesh <ritesh221200@gmail.com>
2020-06-10 17:54:25 +0530

    restructure

--- profiles/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from . models import Profile
-from django.contrib import admin
-
-
-class ProfilesConfig(AppConfig):
-    name = 'profiles'
-
-admin.site.register(Profile)
--- team/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . models import Team
-
-admin.site.register(Team)
