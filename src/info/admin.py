39a0f912053675cf6b3e5c77b22806b28a6b0b29
vinx-2105 <2017csb1119@iitrpr.ac.in>
2020-09-09 00:08:52 +0530

    added more pd pages like initiatives and pg resources

--- src/info/admin.py
+++ src/info/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import News,Events, Resource, ProfessionalDevelopmentActivity, ProfessionalDevelopmentBook, ProfessionalDevelopmentVideo, CorporateRelationsActivity
-from .models import ResourceCategory, Department
+from .models import ResourceCategory, Department, ProfessionalDevelopmentInitiatives
 # Register your models here.
 admin.site.register(News)
 admin.site.register(Events)
@@ -11,3 +11,4 @@ admin.site.register(ProfessionalDevelopmentVideo)
 admin.site.register(CorporateRelationsActivity)
 admin.site.register(ResourceCategory)
 admin.site.register(Department)
+admin.site.register(ProfessionalDevelopmentInitiatives)
--- src/info/professional_development/views.py
+++ src/info/professional_development/views.py
@@ -3,7 +3,7 @@ Views for the professional development section
 """
 
 from django.shortcuts import render
-from info.models import ProfessionalDevelopmentActivity, ProfessionalDevelopmentBook, ProfessionalDevelopmentVideo
+from info.models import ProfessionalDevelopmentActivity, ProfessionalDevelopmentBook, ProfessionalDevelopmentVideo, ProfessionalDevelopmentInitiatives
 from recruiter.models import Recruiter
 from django.db.models import Q
 from config.utils import get_page_visibility_status
@@ -26,12 +26,28 @@ def activities(request):
     }
     return render(request, 'info/professional_development/activities.html', context=context)
 
+def initiatives(request):
+    if(get_page_visibility_status('pd_initiatives')==False):
+        return render(request, 'under_construction.html')
+    context = {
+        'initiatives': ProfessionalDevelopmentInitiatives.objects.order_by('-date'),
+        'title': 'Professional Development Initiatives',
+    }
+    return render(request, 'info/professional_development/initiatives.html', context=context)
+
 def activity_detail(request, pk):
     context = {
         'activity': ProfessionalDevelopmentActivity.objects.get(pk=pk),
         'title': 'Activity Detail',
     }
     return render(request, 'info/professional_development/activity_detail.html', context=context)
+
+def initiative_detail(request, pk):
+    context = {
+        'initiative': ProfessionalDevelopmentInitiatives.objects.get(pk=pk),
+        'title': 'Initiative Detail',
+    }
+    return render(request, 'info/professional_development/initiative_detail.html', context=context)
 
 def books(request):
     if(get_page_visibility_status('pd_library')==False):
@@ -55,3 +71,9 @@ def hod_message(request):
     if(get_page_visibility_status('pd_hod_message')==False):
         return render(request, 'under_construction.html')
     return render(request, 'info/professional_development/hod_message.html')
+
+def pg_resources(request):
+    context = {
+        'title' : 'Resources for PG Students',
+    }
+    return render(request, 'info/professional_development/graduate_student_resources.html', context=context)
