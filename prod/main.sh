f7a59d2a591c174c6125ed49b6854f8eaf39c3b0
Subham Sahu <subham.iitrpr@gmail.com>
2021-05-20 16:36:16 +0000

    Add script to be deployed on the prod server

---
+++ prod/main.sh
@@ -0,0 +1,32 @@
+# script to be run on the server, to deploy the application
+
+# Ensure that $ENV_PATH is set
+# You may use the export command to do like this or update the .bashrc
+# export ENV_PATH="/workspaces/cred/.env"
+
+# Ensure that docker is installed
+
+set -e
+
+echo "Cloning the repo"
+# Remove the directory if it exist
+rm -rf cdcrc_website
+git clone https://github.com/vinx-2105/cdcrc_website
+
+cd cdcrc_website
+
+echo "Moving the env file from the \$ENV_PATH to current directory"
+cp $ENV_PATH ./
+
+sudo chmod +x startup.sh
+
+# Building 
+echo "Build the application"
+docker-compose -f docker-compose.prod.yml build
+
+echo "Start the application"
+docker-compose -f docker-compose.prod.yml up -d
+
+echo "Executing the startup script"
+./startup.sh
+
--- startup.sh
+++ startup.sh
@@ -6,6 +6,15 @@ docker-compose -f docker-compose.prod.yml exec backend python manage.py makemigr
 docker-compose -f docker-compose.prod.yml exec backend python manage.py migrate
 # Calling collectstatic to dump all the static files
 docker-compose -f docker-compose.prod.yml exec backend python manage.py collectstatic --no-input
+
+
 # Creating a superuser using the environment variables 
-docker-compose -f docker-compose.prod.yml exec backend python manage.py createsuperuser --no-input
+docker-compose -f docker-compose.prod.yml exec -T backend python manage.py shell -c "from django.contrib.auth import get_user_model; \
+import os; \
+User = get_user_model(); \
+username=os.environ.get('DJANGO_SUPERUSER_USERNAME'); \
+password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'); \
+email=os.environ.get('DJANGO_SUPERUSER_EMAIL'); \
+User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
+
 
