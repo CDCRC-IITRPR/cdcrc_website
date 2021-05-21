e6be8609cb8c754947530d18ee5f3279d4ce015f
Subham Sahu <subham.iitrpr@gmail.com>
2021-05-21 17:54:38 +0000

    Add choices to PageVisibilityConfig model & update startup.sh with args

--- backend/src/config/models.py
+++ backend/src/config/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
-# Create your models here.
 class PageVisibilityConfig(models.Model):
-    page = models.CharField(max_length=128, null=False, blank=False, unique=True)
+    CHOICES = (
+        ('pd_about', 'pd_about'),
+        ('pd_activities', 'pd_activities'),
+        ('pd_library', 'pd_library'),
+        ('pd_videos', 'pd_videos'),
+        ('cr_about', 'cr_about'), 
+        ('cr_activities', 'cr_activities'),
+        ('cr_hod_message', 'cr_hod_message'),
+        ('pd_hod_message', 'pd_hod_message'),
+        ('tnp_hod_message', 'tnp_hod_message'),
+        ('pd_initiatives', 'pd_initiatives'),
+    )
+    page = models.CharField(max_length=128, null=False, blank=False, unique=True, choices=CHOICES)
     visible = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
--- startup.sh
+++ startup.sh
@@ -1,15 +1,31 @@
 # This script should be executed just after all the contains are built and the started for first time
 
-# Making migrations files
-docker-compose -f docker-compose.prod.yml exec backend python manage.py makemigrations
-# Migrating changes into the database
-docker-compose -f docker-compose.prod.yml exec backend python manage.py migrate
-# Calling collectstatic to dump all the static files
-docker-compose -f docker-compose.prod.yml exec backend python manage.py collectstatic --no-input
+# Two ways to execute the script:
+# ./startup.sh docker-compose.yml --collect-static
+# ./startup.sh docker-compose.yml
 
+if [ -z "$1" ]; 
+    then echo "No File Param Passed";
+    exit 1
+fi
+
+# Making migrations files
+docker-compose -f $1 exec backend python manage.py makemigrations
+# Migrating changes into the database
+docker-compose -f $1 exec backend python manage.py migrate
+
+
+if [ "$2" = "--collect-static" ];
+then 
+    echo "Performing Collect Static"
+    # Calling collectstatic to dump all the static files
+    docker-compose -f $1 exec backend python manage.py collectstatic --no-input
+else
+    echo "Skipping collect static"
+fi
 
 # Creating a superuser using the environment variables 
-docker-compose -f docker-compose.prod.yml exec -T backend python manage.py shell -c "from django.contrib.auth import get_user_model; \
+docker-compose -f $1 exec -T backend python manage.py shell -c "from django.contrib.auth import get_user_model; \
 import os; \
 User = get_user_model(); \
 username=os.environ.get('DJANGO_SUPERUSER_USERNAME'); \
